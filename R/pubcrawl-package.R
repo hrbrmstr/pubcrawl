3bac0d5ded3602e8702df3f5bb50159db6c160d8
boB Rudis <bob@rud.is>
2018-04-12 08:40:10 -0400

    README; DESCRIPTION; passes CRAN checks

--- R/pubcrawl-package.R
+++ R/pubcrawl-package.R
@@ -1,8 +1,12 @@
 #' Convert 'epub' Files to Text
 #'
+#' The 'epub' file format is really just a structured 'ZIP' archive with
+#' metadata, graphics and (usually) 'HTML' text. Tools are provided to turn an 'epub'
+#' file into a tidy data frame.
+#'
 #' @md
 #' @name pubcrawl
 #' @docType package
 #' @author Bob Rudis (bob@@rud.is)
-#' @import purrr dplyr stringi hgr readr
+#' @import purrr dplyr stringi hgr readr archive
 NULL
