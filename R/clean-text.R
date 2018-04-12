da73ca2143e9e77f038bcef7a8fa1f5670fd9f0d
boB Rudis <bob@rud.is>
2018-04-12 17:22:54 -0400

    Experimental encoding support to help #4 along

--- R/clean-text.R
+++ R/clean-text.R
@@ -1,12 +1,15 @@
 # @param doc parsed or character xml / html doc
+# @param encoding encoding
 # @return a single element character vector (hopefully) devoid of markup
-clean_text <- function(doc) {
+clean_text <- function(doc, encoding = "UTF-8") {
 
-  if (!inherits(doc, "html_document")) doc <- xml2::read_html(doc)
+  if (!inherits(doc, "html_document")) {
+    doc <- xml2::read_html(charToRaw(doc), encoding = encoding)
+  }
 
   cleaner <- xml2::read_xml(system.file("xslt/justthetext.xslt", package="pubcrawl"))
 
-  doc_tmp <- xslt::xml_xslt(doc, cleaner)
+  doc_tmp <- xslt::xml_xslt(doc, cleaner, )
   doc_tmp <- rvest::html_text(doc_tmp)
   doc_tmp <- stri_trim_both(doc_tmp)
 
@@ -22,15 +25,16 @@ clean_text <- function(doc) {
 
 # @param bk is the epub book file opened with archive::archive
 # @param .x is the path to slurp up from the book connection
+# @param encoding encoding
 # @return a single element character vector (hopefully) devoid of markup
-clean_epub <- function(bk, .x) {
+clean_epub <- function(bk, .x, encoding = "UTF-8") {
 
   con <- archive::archive_read(bk, .x)
-  out <- readLines(con, warn = FALSE)
+  out <- readLines(con, warn = FALSE, encoding = encoding)
   close(con)
 
   out <- paste0(out, collapse = "\n")
-  out <- clean_text(out)
+  out <- clean_text(out, encoding)
 
   out
 
--- R/epub-to-text.R
+++ R/epub-to-text.R
@@ -1,10 +1,12 @@
 #' Convert an epub file into a data frame of plaintext chapters
 #'
 #' @param path path to epub
+#' @param encoding Some content may require special handling of
+#'        encodings. The default (`UTF-8`) can be overriden with this parameter.
 #' @export
 #' @examples
 #' epub_to_text(system.file("extdat", "augustine.epub", package="pubcrawl"))
-epub_to_text <- function(path) {
+epub_to_text <- function(path, encoding = "UTF-8") {
 
   path <- path.expand(path)
 
@@ -12,10 +14,18 @@ epub_to_text <- function(path) {
 
   bk <- bk[stri_detect_fixed(bk$path, "html"),]
 
-  bk$content <- vapply(bk$path, clean_epub, character(1), bk = path)
+  bk$content <- vapply(
+    bk$path,
+    clean_epub,
+    character(1),
+    bk = path, encoding = encoding
+  )
+
+  bk$content <- iconv(bk$content, "", to = encoding)
 
   class(bk) <- c("tbl_df", "tbl", "data.frame")
 
   bk
 
 }
+
